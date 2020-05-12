8d7e6ec725c1327bbd96a8d87765f4ac072f5116
pankaj <pankaj@localhost.localdomain>
2020-05-12 22:13:40 +0530

    updated machines

--- main.tf
+++ main.tf
@@ -1,37 +1,45 @@
 
 
-resource "libvirt_volume" "k8s-qcow2" {
-  name   = "k8s.qcow2"
-  pool   = "MyVM"
+resource "libvirt_volume" "k8s" {
+  count = length(var.vm_names)
+  name = "${var.vm_names[count.index]}.qcow2"
+  pool = "MyVM"
   #source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
-  source = "/myvm/myvm/master.qcow2"
+  source = "/myvm/myvm/master.qcow2"  #CHANGEME
   format = "qcow2"
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-          name = "commoninit.iso"
-          pool = "MyVM" #CHANGEME
-          user_data = data.template_file.user_data.rendered
-          }
+  name      = "commoninit${count.index}.iso"
+  pool      = "MyVM" #CHANGEME
+  user_data = data.template_file.user_data[count.index].rendered
+   count = length(var.vm_names)
+}
 
 data "template_file" "user_data" {
   template = file("${path.module}/cloud_init.cfg")
+    vars = {
+    HOSTNAME = var.vm_names[count.index]
+  }
+  count = length(var.vm_names)
 }
+
 # Define KVM domain to create
 resource "libvirt_domain" "master" {
-  name   = "master"
+  name   = var.vm_names[count.index]
   memory = "1024"
   vcpu   = 1
 
   network_interface {
-    network_name = "default"
-  
+    network_name   = "default"
+    hostname       = var.vm_names[count.index]
+    wait_for_lease = true
   }
- 
- cloudinit = libvirt_cloudinit_disk.commoninit.id
- 
+
+  cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
+
   disk {
-    volume_id = libvirt_volume.k8s-qcow2.id
+      volume_id = libvirt_volume.k8s[count.index].id
   }
 
   console {
@@ -45,4 +53,9 @@ resource "libvirt_domain" "master" {
     listen_type = "address"
     autoport    = true
   }
+  count = length(var.vm_names)
+}
+
+output "IPs" {
+  value = libvirt_domain.master.*.network_interface.0.addresses
 }
--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "vm_names" {
   description = "The names of the VMs to create"
-  type = list(string)
-  default = ["master","worker01","worker02"]
+  type        = list(string)
+  default     = ["master01", "worker01", "worker02"]
 }
